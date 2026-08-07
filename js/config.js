b52a06603f3a95f9c6284a17b66f0bc44eb16990
yxstudios <sh3cke@gmail.com>
2026-08-07 15:29:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
 // Configuración de Supabase
 const SUPABASE_URL = 'https://xzfytuasxmqxdcdwfbbl.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Znl0dWFzeG1xeGRjZHdmYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNzkyMDcsImV4cCI6MjEwMTY1NTIwN30.QWM-EkPbQxTaWraKzUEQraJJLsgfjwNyxOc1Krh82tU';
+
+// Configuración de PayPal
+const PAYPAL_CLIENT_ID = 'Aa6KFjacVgqjs6vg9uBBD58-DAVVU1QPXJ_RIrDqUhVSXXs_n35A65NRxCcc3kN91D-pgZg6UsKBW05u';
+const PAYPAL_SECRET_KEY = 'EJvyhkZzM6SvqoVSGUksEf5_WmjmYia9SKuhOtJFHyUGEcGCnA9tUnRUOXW1nHk2v14KvAwN2CSonFJI';
