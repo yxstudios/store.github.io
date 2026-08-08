8d86e424edbd7eaf825b286b2df5b5d931a7beb8
yxstudios <sh3cke@gmail.com>
2026-08-08 04:56:31 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 // Configuración de Supabase
-const SUPABASE_URL = 'https://xzfytuasxmqxdcdwfbbl.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Znl0dWFzeG1xeGRjZHdmYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNzkyMDcsImV4cCI6MjEwMTY1NTIwN30.QWM-EkPbQxTaWraKzUEQraJJLsgfjwNyxOc1Krh82tU';
+var SUPABASE_URL = 'https://xzfytuasxmqxdcdwfbbl.supabase.co';
+var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Znl0dWFzeG1xeGRjZHdmYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNzkyMDcsImV4cCI6MjEwMTY1NTIwN30.QWM-EkPbQxTaWraKzUEQraJJLsgfjwNyxOc1Krh82tU';
 
 // Configuración de PayPal
-const PAYPAL_CLIENT_ID = 'Aa6KFjacVgqjs6vg9uBBD58-DAVVU1QPXJ_RIrDqUhVSXXs_n35A65NRxCcc3kN91D-pgZg6UsKBW05u';
-const PAYPAL_SECRET_KEY = 'EJvyhkZzM6SvqoVSGUksEf5_WmjmYia9SKuhOtJFHyUGEcGCnA9tUnRUOXW1nHk2v14KvAwN2CSonFJI';
+var PAYPAL_CLIENT_ID = 'Aa6KFjacVgqjs6vg9uBBD58-DAVVU1QPXJ_RIrDqUhVSXXs_n35A65NRxCcc3kN91D-pgZg6UsKBW05u';
+var PAYPAL_SECRET_KEY = 'EJvyhkZzM6SvqoVSGUksEf5_WmjmYia9SKuhOtJFHyUGEcGCnA9tUnRUOXW1nHk2v14KvAwN2CSonFJI';
 
 // Logos por tema
-const THEME_LOGOS = {
+var THEME_LOGOS = {
     'red': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-1.avif',
     'purple': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-2.avif',
     'blue': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-3.avif',
