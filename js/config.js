c2d3c0e7178943e0877678b8d7378cf20698939f
yxstudios <sh3cke@gmail.com>
2026-08-08 01:46:06 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,3 +5,11 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 // Configuración de PayPal
 const PAYPAL_CLIENT_ID = 'Aa6KFjacVgqjs6vg9uBBD58-DAVVU1QPXJ_RIrDqUhVSXXs_n35A65NRxCcc3kN91D-pgZg6UsKBW05u';
 const PAYPAL_SECRET_KEY = 'EJvyhkZzM6SvqoVSGUksEf5_WmjmYia9SKuhOtJFHyUGEcGCnA9tUnRUOXW1nHk2v14KvAwN2CSonFJI';
+
+// Logos por tema
+const THEME_LOGOS = {
+    'red': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-1.avif',
+    'purple': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-2.avif',
+    'blue': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-3.avif',
+    'dark-gray': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-4.avif'
+};
