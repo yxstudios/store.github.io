e1bcef28b50692605de6643c3a90829f0a1c82ae
yxstudios <sh3cke@gmail.com>
2026-08-08 05:08:24 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,7 @@
-// Configuración de Supabase
 var SUPABASE_URL = 'https://xzfytuasxmqxdcdwfbbl.supabase.co';
-var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Znl0dWFzeG1xeGRjZHdmYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNzkyMDcsImV4cCI6MjEwMTY1NTIwN30.QWM-EkPbQxTaWraKzUEQraJJLsgfjwNyxOc1Krh82tU';
-
-// Configuración de PayPal
+var SUPABASE_ANON_KEY = 'sb_publishable_sjdKZM4PVvfp5pA5sTlSbg_F-_UJLgX';
 var PAYPAL_CLIENT_ID = 'Aa6KFjacVgqjs6vg9uBBD58-DAVVU1QPXJ_RIrDqUhVSXXs_n35A65NRxCcc3kN91D-pgZg6UsKBW05u';
 var PAYPAL_SECRET_KEY = 'EJvyhkZzM6SvqoVSGUksEf5_WmjmYia9SKuhOtJFHyUGEcGCnA9tUnRUOXW1nHk2v14KvAwN2CSonFJI';
-
-// Logos por tema
 var THEME_LOGOS = {
     'red': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-1.avif',
     'purple': 'https://user21990.na.imgto.link/public/20260808/chatgpt-image-8-ago-2026-01-30-31-a-m-2.avif',
